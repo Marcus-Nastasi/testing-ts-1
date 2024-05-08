3762a231eaa01e884a278eb224e11b195d3de25a
rolemberg <vinnie.nsts@gmail.com>
2024-05-07 23:35:55 -0300

    configuring tailwind framework

--- out/index.js
+++ out/index.js
@@ -2,9 +2,10 @@
 function showHello() {
     const section = document.getElementById('secHeaderTop');
     const p = document.createElement('p');
-    p.innerText = `Wellcome to the TS website, guest! Feel free to play.`;
+    const wellcome = `Wellcome to the TS website, guest! Feel free to play.`;
+    p.innerText = wellcome;
     section === null || section === void 0 ? void 0 : section.appendChild(p);
 }
 ;
-window.onload = showHello;
+window.onload = () => setTimeout(() => showHello(), 2000);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 function showHello(): void {
    const section: HTMLElement | null = document.getElementById('secHeaderTop');
    const p: HTMLParagraphElement = document.createElement('p');
-   p.innerText = `Wellcome to the TS website, guest! Feel free to play.`;
+   const wellcome: string = `Wellcome to the TS website, guest! Feel free to play.` 
+   p.innerText = wellcome;
    section?.appendChild(p); 
 };
 
-window.onload = showHello;
+window.onload = () => setTimeout(() => showHello(), 2000);
+
 
