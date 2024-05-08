e8e81ab6104995dc0fd83af740f80b92a49f90a3
rolemberg <vinnie.nsts@gmail.com>
2024-05-07 23:22:08 -0300

    testing html elements on TS

--- out/index.js
+++ out/index.js
@@ -1,4 +1,10 @@
 "use strict";
-const nmr = 10;
-console.log(nmr);
+function showHello() {
+    const section = document.getElementById('secHeaderTop');
+    const p = document.createElement('p');
+    p.innerText = `Wellcome to the TS website, guest! Feel free to play.`;
+    section === null || section === void 0 ? void 0 : section.appendChild(p);
+}
+;
+window.onload = showHello;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-const nmr: number = 10;
-console.log(nmr);
+function showHello(): void {
+   const section: HTMLElement | null = document.getElementById('secHeaderTop');
+   const p: HTMLParagraphElement = document.createElement('p');
+   p.innerText = `Wellcome to the TS website, guest! Feel free to play.`;
+   section?.appendChild(p); 
+};
+
+window.onload = showHello;
+
