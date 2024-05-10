6715aa550c527d511f6f78d5df34cd94644ec9c2
rolemberg <vinnie.nsts@gmail.com>
2024-05-10 11:00:17 -0300

    typing IntersectionObserver func;

--- src/ts/transitions.ts
+++ src/ts/transitions.ts
@@ -1,11 +1,11 @@
 function slide(): void {
-   const slides = document.getElementsByClassName('slide')[0];
+   const slides: any = document.getElementsByClassName('slide')[0];
    const [ slideL, slideR ]: any = [ 
       document.getElementsByClassName('slideLeft'),
       document.getElementsByClassName('slideRigth') 
    ];
    
-   const observer = new window.IntersectionObserver(([entry]) => {
+   const observer: IntersectionObserver = new window.IntersectionObserver(([entry]: any): void => {
       if(entry.isIntersecting) {
          slideL[0].style.opacity = '1';
          slideR[0].style.opacity = '1';
