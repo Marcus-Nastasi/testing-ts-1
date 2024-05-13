9443cb8dc0a4ecf944fb336c2faf81cd7e85ad18
rolemberg <vinnie.nsts@gmail.com>
2024-05-13 00:24:19 -0300

    debuggin carrossel function;

--- src/ts/carrossel.ts
+++ src/ts/carrossel.ts
@@ -1,30 +1,33 @@
 const [ mv1, mv2, mv3, mv4, mv5, mv6, mv7, mv8, mv9, mv10 ]: any = document.getElementsByClassName('carr');
 
 const moveOne = () => {
+   if(mv1.classList.contains('mvOut')) mv1.classList.remove('mvOut');
    mv1.classList.add('mvUp');
    setTimeout(() => {
-      mv1.classList.remove('mvUp');
       mv1.classList.add('mvOut');
+      mv1.classList.remove('mvUp');
       movetwo();
-   }, 1000);
+   }, 3000);
 };
 
 const movetwo = () => {
+   if(mv2.classList.contains('mvOut')) mv2.classList.remove('mvOut');
    mv2.classList.add('mvUp');
    setTimeout(() => {
-      mv2.classList.remove('mvUp');
       mv2.classList.add('mvOut');
+      mv2.classList.remove('mvUp');
       moveThree();
-   }, 1000);
+   }, 3000);
 };
 
 const moveThree = () => {
+   if(mv2.classList.contains('mvOut')) mv2.classList.remove('mvOut');
    mv3.classList.add('mvUp');
    setTimeout(() => {
-      mv3.classList.remove('mvUp');
       mv3.classList.add('mvOut');
+      mv3.classList.remove('mvUp');
       moveOne();
-   }, 1000);
+   }, 3000);
 };
 moveOne();
 
